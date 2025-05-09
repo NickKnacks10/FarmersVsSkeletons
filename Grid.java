9140668a2148918e2d90fa54de077b1b1b63ddbd
NickKnacks10 <nickers090506@gmail.com>
2025-05-09 09:52:55 -0500

    Update

--- Grid.java
+++ Grid.java
@@ -1,10 +1,21 @@
 public class Grid{
     private Icon sprite;
 
-    public Grid(String sprite){
-        this.sprite = sprite;
+    public Grid(String spriteStrFirstRow, String spriteStrSecondRow, String spriteStrThirdRow){
+        sprite = new Icon(spriteStrFirstRow,spriteStrSecondRow,spriteStrThirdRow);
     }
 
+    //Prerequisite: pos == 0, 1, or 2 
+    public String getSpriteParts(int pos){
+        if(pos == 0){
+            return sprite.getFirst();
+        }
+        if(pos == 1){
+            return sprite.getSecond();
+        }
+        return sprite.getThird();
+    }
+    // Broken right now
     public String toString(){
         return "| +"sprite+" |";
     }
--- Main.java
+++ Main.java
@@ -2,14 +2,23 @@ public class Main{
   public static void main(String[] args){
     System.out.println("Field Test");
     Grid[][] field = new Grid[5][9];
+    //Just testing out the field generation
+    for(int row = 0; row < field.length; row++){
+      for(int col = 0; col < field[0].length; col++){
+        Grid[row][col] = new Grid("I I", "I_I", "I I");
+      }
+    }
+    //Printing out the field
     for(int row = 0; row < field.length; row++){ 
       for(int sectRow = 0; sectRow < 3; sectRow++){
         for(int col = 0; col < field[0].length; col++){
-          System.out.print(Grid[sectRow][col] + "   ");
+          System.out.print(Grid[sectRow][col].getSpriteParts(sectRow) + "   ");
         }
         System.out.println();
       }
       System.out.println("\n");
     }
+
+
   }
 }
