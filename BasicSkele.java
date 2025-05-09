16826c3ede77649d4502a8987f928fb86253f76c
NickKnacks10 <nickers090506@gmail.com>
2025-05-09 13:55:20 -0500

    Update

--- BasicSkele.java
+++ BasicSkele.java
@@ -1,5 +1,10 @@
 public class BasicSkele extends Skele{
     public BasicSkele(){
-        super("")
+        super(
+        "  X  ", 
+        "  |  ", 
+        "  ^  ",
+        //health, speed, damage, attackSpeed
+        250, 100, 50, 100);
     }
 }
--- Skele.java
+++ Skele.java
@@ -1,15 +1,23 @@
 public class Skele extends Grid{
     private int health;
-    private int speed;
+    private int speed; //As a percent
     private int damage;
     private int attackSpeed;
 
-    public Skele(String spriteStrFirstRow, String spriteStrSecondRow, String spriteStrThirdRow){
+    public Skele(String spriteStrFirstRow, String spriteStrSecondRow, String spriteStrThirdRow, int health, int speed, int damage, int attackSpeed){
         super(spriteStrFirstRow, spriteStrSecondRow, spriteStrThirdRow);
+        this.health = health;
+        this.speed = speed;
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
     }
 
     public void attack(){
         //
+    }
+
+    public void loseHealth(int att){
+        health -= att;
     }
 
     public void move(){
