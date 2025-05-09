852571b787149888adce3389a690d52cb1dc4075
NickKnacks10 <nickers090506@gmail.com>
2025-05-09 07:45:41 -0500

    Update

--- Main.java
+++ Main.java
@@ -2,10 +2,14 @@ public class Main{
   public static void main(String[] args){
     System.out.println("Field Test");
     Grid[][] field = new Grid[5][9];
-    for(int row = 0; row < field.length; row++){
-      for(int col = 0; col < field[0].length; col++){
-        
+    for(int row = 0; row < field.length; row++){ 
+      for(int sectRow = 0; sectRow < 3; sectRow++){
+        for(int col = 0; col < field[0].length; col++){
+          System.out.print(Grid[sectRow][col] + "   ");
+        }
+        System.out.println();
       }
+      System.out.println("\n");
     }
   }
 }
