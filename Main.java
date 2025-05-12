2b016d18e312d66a26fa99a4e44db6538334418a
NickKnacks10 <nickers090506@gmail.com>
2025-05-12 09:45:35 -0500

    Update

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main{
+public class Main extends Thread{
   public static void main(String[] args){
     System.out.println("Field Test");
     Grid[][] field = new Grid[5][9];
@@ -9,20 +9,32 @@ public class Main{
         field[row][col] = new Grid("     ", " ___ ", "     ");
       }
     }
+    //Testing threads here; No idea if this works yet
+    Main thread = new Main();
+    thread.start();
+
     //Printing out the field
-    for(int row = 0; row < field.length; row++){ 
-      for(int sectRow = 0; sectRow < 3; sectRow++){
-        for(int col = 0; col < field[0].length; col++){
-          System.out.print("|" + field[sectRow][col].getSpriteParts(sectRow) + "|   ");
+    while(true){}
+      for(int row = 0; row < field.length; row++){ 
+        for(int sectRow = 0; sectRow < 3; sectRow++){
+          for(int col = 0; col < field[0].length; col++){
+            System.out.print("|" + field[sectRow][col].getSpriteParts(sectRow) + "|   ");
+          }
+          System.out.println();
         }
-        System.out.println();
+        System.out.println("\n");
       }
-      System.out.println("\n");
+      Thread.sleep(5000);
+      clearConsole();
     }
   }
 
   public static void clearConsole(){ //May or may not work; have to test at home
     System.out.print("\033[H\033[2J");
     System.out.flush();
+  }
+
+  public void run(){
+    System.out.println("Thread testing");
   }
 }
