7c7718cf69de07cc4286ec488a764427200778e7
NickKnacks10 <nickers090506@gmail.com>
2025-05-12 10:45:36 -0500

    Experimental Update

--- Main.java
+++ Main.java
@@ -2,12 +2,31 @@ public class Main extends Thread{
   public static void main(String[] args){
     System.out.println("Field Test");
     //Maybe change to a 2D array of Arraylists containing Grid objects to handle multiple skeletons of the same tile
+    
     Grid[][] field = new Grid[5][9];
+    
+    //Attempt 1 to make 2d array of arraylists
+    /*
+    Arraylist<Grid>[][] field = new Arraylist<Grid>[5][9];
+    for(int row = 0; row < field.length; row++){
+      for(int col = 0; col < field[0].length; col++){
+        field[row][col] = new ArrayList<Grid>();
+      }
+    }
+
+    for(int row = 0; row < field.length; row++){
+      for(int col = 0; col < field[0].length; col++){
+        //5 Characters long?
+        field[row][col].add(new Grid("     ", "     ", "     "));
+      }
+    }
+    */
+
     //Just testing out the field generation
     for(int row = 0; row < field.length; row++){
       for(int col = 0; col < field[0].length; col++){
         //5 Characters long?
-        field[row][col] = new Grid("     ", " ___ ", "     ");
+        field[row][col] = new Grid("     ", "     ", "     ");
       }
     }
     //Testing threads here; No idea if this works yet
