0729e6fbc96312c6f672842d1c6e94bae7914716
Nikolai Nelson <nikolai.nelson10@gmail.com>
2025-05-12 19:55:07 -0500

    Update

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-public class Main extends Thread{
+import java.util.ArrayList;
+
+public class Main{
   public static void main(String[] args){
-    System.out.println("Field Test");
     //Maybe change to a 2D array of Arraylists containing Grid objects to handle multiple skeletons of the same tile
     
-    Grid[][] field = new Grid[5][9];
-    
-    //Attempt 1 to make 2d array of arraylists
-    /*
-    Arraylist<Grid>[][] field = new Arraylist<Grid>[5][9];
+    ArrayList<Grid>[][] field = new ArrayList[5][9];
     for(int row = 0; row < field.length; row++){
       for(int col = 0; col < field[0].length; col++){
         field[row][col] = new ArrayList<Grid>();
@@ -17,28 +14,16 @@ public class Main extends Thread{
     for(int row = 0; row < field.length; row++){
       for(int col = 0; col < field[0].length; col++){
         //5 Characters long?
-        field[row][col].add(new Grid("     ", "     ", "     "));
+        field[row][col].add(new Grid("     ", " ___ ", "     "));
       }
     }
-    */
-
-    //Just testing out the field generation
-    for(int row = 0; row < field.length; row++){
-      for(int col = 0; col < field[0].length; col++){
-        //5 Characters long?
-        field[row][col] = new Grid("     ", " ___ ", "     ");
-      }
-    }
-    //Testing threads here; No idea if this works yet
-    Main thread = new Main();
-    thread.start();
 
     //Printing out the field
     while(true){
       for(int row = 0; row < field.length; row++){ 
         for(int sectRow = 0; sectRow < 3; sectRow++){
           for(int col = 0; col < field[0].length; col++){
-            System.out.print("|" + field[sectRow][col].getSpriteParts(sectRow) + "|   ");
+            System.out.print("|" + field[sectRow][col].get(0).getSpriteParts(sectRow) + "|   ");
           }
           System.out.println();
         }
@@ -59,8 +44,6 @@ public class Main extends Thread{
   public static void clearConsole(){ //Thank you, random Youtube Shorts youtuber (adam bien)
     System.out.print("\033c"); 
   }
-
-  public void run(){
-    System.out.println("Thread testing");
-  }
 }
+
+//Note to self: Threads do in fact work
