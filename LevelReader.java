a775be1dd87cb1fd844d218c74c4e543e77099fb
Nikolai Nelson <nikolai.nelson10@gmail.com>
2025-06-02 06:01:55 -0500

    Update

--- LevelReader.java
+++ LevelReader.java
@@ -3,6 +3,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LevelReader implements Runnable{
+    
+    static int skeleCount = 0;
+    static int maxSkeleCount = 0;
+    
     public void run(){
         levelRunner();
     }
@@ -11,15 +15,14 @@ public class LevelReader implements Runnable{
         try {
             File level = new File("Level1.txt");
             Scanner levelReader = new Scanner(level);
-            int skeleCount = 0;
-            int maxSkeleCount = 0;
+            
             while (levelReader.hasNextLine()) {
                 String data = levelReader.nextLine();
-                if(data.substring(0,1).equals("*") || data.equals("")){
+                if(data.substring(0,1).equals("*")){
                     continue;
                 } else if(data.substring(0,4).equals("cre")){
                     //Make this a custom method with the txt files starting with "create"?
-                    createSkele(data.substring(5,9));
+                    createSkele(Integer.parseInt(data.substring(5,9)), data.substring(9,10));
                     //Check if this actually works
                     
                 } else if(data.substring(0,3).equals("end")){
@@ -35,38 +38,42 @@ public class LevelReader implements Runnable{
                                 System.out.println("error: wave end timer failed");
                             }
                         }
+                        maxSkeleCount = 0;
+                        skeleCount = 0;
                     }
                 }
             }
+            System.out.println("LevelEnd");
         } catch (FileNotFoundException e) {
             System.out.println("Something goofed up.");
             e.printStackTrace();
         }
     }
 
-    public static void createSkele(int identity){
-        Skele ske;
+    public static void createSkele(int identity, String rowId){
+        Skele ske = null;
         try{
             switch(identity){
-                case "0001"{
+                case 1:
                     ske = new BasicSkele();
                     break;
-                }
-                case "0002"{
+                
+                case 2:
                     ske = new LeatherSkele();
                     break;
-                }
-                case "0003"{
+                
+                case 3:
                     ske = new IronSkele();
                     break;
-                }
+                
             }
-            int rowThing = Integer.parseInt(data.substring(5,6));
+            int rowThing = Integer.parseInt(rowId);
             Main.field[rowThing][8].add(ske);
             ske.setPosition(rowThing, 8);
             Thread thr = new Thread(ske);
             thr.start();
             skeleCount++;
+            maxSkeleCount++;
         } catch (Exception e){
             System.out.println("Skele creation failed");
         }
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main{
   
   public static ArrayList<Grid>[][] field = new ArrayList[5][9];
-  public boolean alive = true;
+  public static boolean alive = true;
 
   public static void main(String[] args){
     //Creating field
@@ -37,7 +37,7 @@ public class Main{
       }
       //To clarify: This try/catch was added by an AI
       try {
-        Thread.sleep(500);
+        Thread.sleep(5000);
       } catch (InterruptedException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
@@ -51,7 +51,7 @@ public class Main{
   }
 
   public static void clearConsole(){ //Thank you, random Youtube Shorts youtuber (adam bien)
-    System.out.print("\033c"); 
+    //System.out.print("\033c"); 
   }
 
 }
